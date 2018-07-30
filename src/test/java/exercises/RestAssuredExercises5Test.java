7a4e8cc531d162b0f02a50c71311bf360c3d5956
beeMonkey <675383749@qq.com>
2018-07-30 15:06:55 +0800

    finish the checkThirdSpeedRecordWasSetIn1955 test

--- src/test/java/exercises/RestAssuredExercises5Test.java
+++ src/test/java/exercises/RestAssuredExercises5Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 public class RestAssuredExercises5Test {
 
@@ -33,7 +34,9 @@ public class RestAssuredExercises5Test {
 		given().
 			spec(requestSpec).
 		when().
-		then();
+				get("/xml/speedrecords").
+		then().
+				body("speedRecords.car[2].year",is("1955"));
 	}
 	
 	/*******************************************************
