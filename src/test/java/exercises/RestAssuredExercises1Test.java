08c2cd4f87cd9a9c7aa6861495dc0349cf386704
beeMonkey <675383749@qq.com>
2018-07-30 11:01:41 +0800

    finish the checkResponseCodeForIncorrectRequest test

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -45,11 +45,11 @@ public class RestAssuredExercises1Test {
     @Test
     public void checkResponseCodeForIncorrectRequest() {
 
-//        given().
-//                spec(requestSpec).
-//                when().
-//                get("/incorrect.json").
-//                then().statusCode(404);
+        given().
+                spec(requestSpec).
+                when().
+                get("/incorrect.json").
+                then().statusCode(404);
     }
 
     /*******************************************************
