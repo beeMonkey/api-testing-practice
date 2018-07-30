2252f84a1b3a103a9973399982cc6ef9deb7427c
beeMonkey <675383749@qq.com>
2018-07-30 11:13:04 +0800

    finish the checkTheFirstRaceOf2014WasAtAlbertPark test

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 
 public class RestAssuredExercises1Test {
@@ -87,7 +88,10 @@ public class RestAssuredExercises1Test {
         given().
                 spec(requestSpec).
                 when().
-                then();
+                get("/2014/1/circuits.json").
+                then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits[0].circuitId",is("albert_park"));
     }
 
     /***********************************************
